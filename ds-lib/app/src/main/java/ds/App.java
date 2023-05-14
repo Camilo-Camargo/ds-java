c7c806a88bbc82ca6ebed6cc9af46906612f5e17
camilo-camargo <camilocamargo49@gmail.com>
2023-05-14 18:09:03 -0500

    Add generic node.

--- ds-lib/app/src/main/java/ds/App.java
+++ ds-lib/app/src/main/java/ds/App.java
@@ -2,6 +2,7 @@ package ds;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Getting Started with data structure.");
+        Node<Integer> integer = new Node<Integer>(10);
+        System.out.println(integer.getValue());
     }
 }
