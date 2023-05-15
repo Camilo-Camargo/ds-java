d5f787f85a2d14b539f25591eb2794cddac782fd
camilo-camargo <camilocamargo49@gmail.com>
2023-05-15 16:02:35 -0500

    Override equals and hashcode functions of Node.

--- ds-lib/app/src/main/java/ds/App.java
+++ ds-lib/app/src/main/java/ds/App.java
@@ -2,19 +2,8 @@ package ds;
 
 public class App {
     public static void main(String[] args) {
-        LinkedList<Integer> linkedList = new LinkedList<Integer>();
-        linkedList.add(2);
-        linkedList.remove(1);
-        linkedList.add(2);
-        linkedList.add(5);
-        linkedList.add(3);
-        linkedList.remove(3);
-        //System.out.println(linkedList.gethead().getValue());
-        Node<Integer> node = linkedList.gethead();
-
-        while(node != null){
-            System.out.println(node.getValue());
-            node = node.getNext();
-        }
+        Node<Integer> node = new Node<Integer>(10);
+        System.out.println(node.equals(new Node<Integer>(10)));
+        System.out.println(new Node<Integer>(10).hashCode());
     }
 }
--- ds-lib/app/src/main/java/ds/Node.java
+++ ds-lib/app/src/main/java/ds/Node.java
@@ -35,4 +35,23 @@ public class Node<T>{
     public Node<T> getPrevious() {
         return previous;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == this){
+            return true;
+        }
+
+        if(!(obj instanceof Node)){
+            return false;
+        }
+
+        Node<T> node = (Node<T>) obj;
+        return node.getValue() == this.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * 7 + value.hashCode();
+    }
 }
